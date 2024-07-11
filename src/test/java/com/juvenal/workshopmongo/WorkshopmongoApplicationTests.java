0cb363d18c4f5c7faabdf41f2e99913e7b832301
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-07-11 15:17:11 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/juvenal/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juvenal.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
