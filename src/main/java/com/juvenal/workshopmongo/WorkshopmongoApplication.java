c679d71d064910dd6baeaa0723b6ecc85e38e6da
Juvenal Silveira da Silva Neto <silveiraneto@hotmail.com>
2024-07-18 17:12:43 -0300

    Incluido configuracao Dotenv

--- src/main/java/com/juvenal/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/juvenal/workshopmongo/WorkshopmongoApplication.java
@@ -3,10 +3,14 @@ package com.juvenal.workshopmongo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class WorkshopmongoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MONGODB_URI", dotenv.get("MONGODB_URI"));
 		SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
 
